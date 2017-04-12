7094bfd2c4cd9d9ddff5ab1f0380c1702eef229d
NIC619 <twedusuck@gmail.com>
2017-04-12 21:19:09 +0200

    Initial Commit

---
+++ SubscriMerger/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SubscriMerger
+//
+//  Created by Nicholas on 2017/4/12.
+//  Copyright © 2017年 NIC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
